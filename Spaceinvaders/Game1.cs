6877ecb8068a259ce2c390490788db39548b0c7e
hellwilliam <gbourscheidt@hotmail.com>
2015-06-16 21:40:38 -0300

    Jogo Inicial

---
+++ Spaceinvaders/Game1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Spaceinvaders
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager m_graphics;
+        SpriteBatch m_spriteBatch;
+
+        Vector2 m_screenRes = new Vector2(800.0f, 600.0f);
+
+        SpriteFont m_font;
+
+        public Game1()
+        {
+            m_graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            m_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Spaceinvaders/Player.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace Spaceinvaders
+{
+    public class Player
+    {
+        Texture2D m_TexPlayer;
+
+        Vector2 m_PlayerPos;
+        Vector2 m_PlayerVel;
+
+        Game1 m_world;
+
+        public void LoadContent()
+        {
+            m_TexPlayer = m_world.Content.Load <Texture2D>("player");
+        }
+
+        public void Update() 
+        {
+            Vector2 dir = Vector2.Zero;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                dir.X -= 1.0f;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                dir.X += 1.0f;
+        }
+
+        public void Draw(GameTime gametime)
+        {
+
+        }
+    }
+}
